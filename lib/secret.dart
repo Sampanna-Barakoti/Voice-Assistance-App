c2d2f8d93f889dee5303c32e1369bac43e6b9892
Sampanna-Barakoti <barakotisampu55@gmail.com>
2025-06-22 18:42:47 +0545

    api key is safe

--- lib/secret.dart
+++ lib/secret.dart
@@ -1,2 +1,3 @@
-const apikey =
-    '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';
+const String apikey = String.fromEnvironment(
+  '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',
+);
